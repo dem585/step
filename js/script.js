54c4607cadc2a8f3438ee310959606017d4addb9
dem585 <65715072+dem585@users.noreply.github.com>
2021-07-27 08:18:58 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,3 +18,24 @@ function outNum(num, elem) {
 };
 
 outNum(548, '#out'); 
+
+$(document).ready(function() {
+var url = "../thank.html";
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(location).attr('href',url);
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
